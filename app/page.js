a749e328fdfa5348ebfdaa11e04d2e8f9b3dee77
vedverma12 <vedv846@gmail.com>
2026-09-11 23:55:30 +0530

    base

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <section className="bg-[#254f1A] min-h-screen">
+
+      </section>
+    </main>
+  );
+}
